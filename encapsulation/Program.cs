c00af456c90bceb689914613046e6e91c51c4469
itsmahammad <mahammadjm-azmpa201@code.edu.az>
2025-10-28 05:37:43 +0400

    Add project files.

---
+++ encapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using encapsulation;
+
+int id;
+
+User[] users = new User[3];
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Entern name: ");
+    string fullname = Console.ReadLine();
+    Console.WriteLine("Entern email: ");
+    string email = Console.ReadLine();
+    Console.WriteLine("Entern age: ");
+    int age = int.Parse(Console.ReadLine());
+
+    users[i] = new User(string fullname, string email, int age);
+}
+
+
+
+
+
+
+Console.WriteLine("1. Show all users \n 2. Get info by id \n 0. Quit");
+
+int choice = Convert.ToInt32(Console.ReadLine());
+
---
+++ encapsulation/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+    internal class User
+    {
+        private int _Id;
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+
+        private static int _Counter = 0;
+        public int Id
+        {
+            get { return _Id; }
+        }
+
+        public User(int id, string fullname, string email, string password, int age)
+        {
+            _Id = id;
+            Fullname = fullname;
+            Email = email;
+            Password = password;
+            Age = age;
+            _Counter++;
+            id = _Counter;
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            if (password.Length >= 8 && password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Id: {_Id}, Fullname: {Fullname}, Email: {Email}");
+        }
+
+    }
+}
